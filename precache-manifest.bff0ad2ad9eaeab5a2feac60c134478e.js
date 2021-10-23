01fe87e4c2446fea0f181a47325e91442adf7ce1
YashKushwaha27 <yashkkushwaha@gmail.com>
2021-10-24 00:20:08 +0530

    Updates

---
+++ precache-manifest.bff0ad2ad9eaeab5a2feac60c134478e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d717b93e22fec29361bfd2f549d123",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a4beca4546a68fdde3c2",
+    "url": "/Portfolio/static/css/main.7466c8ad.chunk.css"
+  },
+  {
+    "revision": "55773d08530795a9c02d",
+    "url": "/Portfolio/static/js/2.9f8562be.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/Portfolio/static/js/2.9f8562be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4beca4546a68fdde3c2",
+    "url": "/Portfolio/static/js/main.3e007850.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "143023f8400866ef6ea3949c508509ad",
+    "url": "/Portfolio/static/media/bitmoji.143023f8.png"
+  },
+  {
+    "revision": "71d1092298d39d5db9b47c5145dd9fc4",
+    "url": "/Portfolio/static/media/dp.71d10922.jpg"
+  },
+  {
+    "revision": "e8c1a5c788fb5b7843e05814d39fa80b",
+    "url": "/Portfolio/static/media/judgementFreeZone.e8c1a5c7.png"
+  },
+  {
+    "revision": "dc99bcabcec520e71053e46e021dda6a",
+    "url": "/Portfolio/static/media/main-background-2.dc99bcab.jpg"
+  },
+  {
+    "revision": "e6c98e900af527887426b747e98a290b",
+    "url": "/Portfolio/static/media/react-redux-thunk.e6c98e90.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/Portfolio/static/media/react-redux.a8b012a0.jpg"
+  }
+]);
